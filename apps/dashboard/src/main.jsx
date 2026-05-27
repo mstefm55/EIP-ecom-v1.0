919034cf8caefc9b82b0dec11f00105058a468aa
mstefm55 <mstefm55@gmail.com>
2026-05-28 03:21:55 +0400

    Route bootstrap links to QR setup page

--- apps/dashboard/src/main.jsx
+++ apps/dashboard/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BootstrapPage from './pages/BootstrapPage.jsx'
+
+const pathname = window.location.pathname.replace(/\/+$/, '')
+const Root = pathname === '/bootstrap' ? BootstrapPage : App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
